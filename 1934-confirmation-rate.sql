cc567b2910d42511c8e6b80ef3de538daa6d8c66
cindymfa <cindymfa45@gmail.com>
2024-12-09 06:30:46 +0700

    Time: 729 ms (79.87%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate.sql
@@ -0,0 +1,8 @@
+-- The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages.
+-- The confirmation rate of a user that did not request any confirmation messages is 0. 
+-- Round the confirmation rate to two decimal places.
+SELECT s.user_id, ROUND(AVG(IF(c.action="confirmed",1,0)),2) as confirmation_rate
+FROM Signups as s
+LEFT JOIN Confirmations as c
+USING(user_id)
+GROUP BY 1;
