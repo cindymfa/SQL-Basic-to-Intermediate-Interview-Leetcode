e82c20c5ae49a522eb50d91a4641ddbaa9d59608
Cindy Meilany Fitri Andriani <86613828+cindymfa@users.noreply.github.com>
2025-01-25 13:40:41 +0700

    delete testing commit

--- 1683-invalid-tweets/a.py
+++
@@ -1,9 +0,0 @@
-{
-    #Is Git enabled
-    "git.enabled": True,
-
-    # // Path to the Git executable
-    "git.path": "C:\\path\\to\\git.exe"
-
-    # // Other settings
-}
--- 1683-invalid-tweets/yes.py
+++
@@ -1,4 +0,0 @@
-a=2
-b=3
-if a==2:
-    print("Hell yes")
