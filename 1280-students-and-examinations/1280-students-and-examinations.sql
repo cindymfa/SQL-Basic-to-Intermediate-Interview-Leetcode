a12c73f86700874de3ba732eb81721660b5e4ac2
Cindy Meilany Fitri Andriani <86613828+cindymfa@users.noreply.github.com>
2025-02-02 00:30:54 +0700

    join 3 tables- Time: 1026 ms (82.09%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++
@@ -1,9 +0,0 @@
--- find the number of times each student attended each exam.
-SELECT a.student_id, a.student_name, b.subject_name, count(e.subject_name) as attended_exams
-FROM Students a
-CROSS JOIN Subjects b
-LEFT JOIN Examinations e
-ON a.student_id=e.student_id AND b.subject_name=e.subject_name
-GROUP BY a.student_id, a.student_name, b.subject_name
-ORDER BY a.student_id, b.subject_name;
-
