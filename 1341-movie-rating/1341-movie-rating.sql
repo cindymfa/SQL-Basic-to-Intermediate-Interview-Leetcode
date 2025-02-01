5c111cd43cb28025e6cb2d1a8dd2dafd2cfc9bc7
Cindy Meilany Fitri Andriani <86613828+cindymfa@users.noreply.github.com>
2025-02-01 23:52:51 +0700

    Time: 2270 ms (37.21%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,31 +1,22 @@
-(
-    select
-        Users.name as results
-    from 
-        MovieRating left join Users
-    on 
-        MovieRating.user_id = Users.user_id 
-    group by
-        Users.user_id
-    order by
-        count(MovieRating.movie_id) desc, 
-        Users.name
-    limit 1
-) 
+-- Find the name of the user who has rated the greatest number of movies.
+-- ---- lexicographically -> SORTED ALPHABETICALLY smaller user name.
+-- Find the movie name with the highest average rating in February 2020. 
+-- ---- smaller movie name.
+(select Users.name as results
+from MovieRating 
+left join Users
+using(user_id)
+group by Users.user_id
+order by count(MovieRating.movie_id) desc, Users.name
+limit 1)
+
 union all
-(
-    select 
-        Movies.title as results
-    from 
-        MovieRating  left join Movies 
-    on
-        MovieRating.movie_id = Movies.movie_id
-    where 
-        MovieRating.created_at like '2020-02%'
-    group by
-        MovieRating.movie_id
-    order by
-        avg(MovieRating.rating) desc,
-        Movies.title 
-    limit 1
-)
+
+(select m.title
+from Movies m
+right join MovieRating mr
+using(movie_id)
+where mr.created_at like '2020-02%'
+group by mr.movie_id
+order by avg(mr.rating) desc, m.title
+limit 1)
