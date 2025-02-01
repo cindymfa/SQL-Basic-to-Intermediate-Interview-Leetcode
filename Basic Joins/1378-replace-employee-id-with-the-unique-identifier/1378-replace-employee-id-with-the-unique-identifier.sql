069f59077a368d54840ba849ad54c2031106c462
Cindy Meilany Fitri Andriani <86613828+cindymfa@users.noreply.github.com>
2025-02-01 23:08:40 +0700

    Time: 1260 ms (84.93%), Space: 0B (100%) - LeetHub

--- Basic Joins/1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ Basic Joins/1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,5 @@
-# Write your MySQL query statement below
+-- Write a solution to show the unique ID of each user
+-- If a user does not have a unique ID replace just show null.
 SELECT u.unique_id, e.name
 FROM EmployeeUNI u
 RIGHT JOIN Employees e
