b7eb9a9915c5623b774c3ab5ef4eadea19db6f13
Cindy Meilany Fitri Andriani <86613828+cindymfa@users.noreply.github.com>
2025-02-02 01:06:54 +0700

    Time: 572 ms (90.57%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products.sql
+++
@@ -1,6 +0,0 @@
---  report the customer ids from the Customer table
--- that bought all the products in the Product table.
-select customer_id 
-from customer 
-GROUP BY customer_id
-HAVING count(distinct product_key) = (select count(*) from Product)
