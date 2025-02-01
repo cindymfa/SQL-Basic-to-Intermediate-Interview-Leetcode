1dcb4cf38efc00390f602164137942417de6f87c
Cindy Meilany Fitri Andriani <86613828+cindymfa@users.noreply.github.com>
2025-02-02 00:58:20 +0700

    Time: 1293 ms (77.16%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii.sql
+++
@@ -1,9 +0,0 @@
--- select the product id, year, quantity, and price for the first year of every product sold.
-SELECT product_id, year AS first_year, quantity, price
-FROM Sales
-WHERE (product_id, year) in (
-    SELECT product_id, MIN(year) 
-    FROM Sales
-    GROUP BY product_id)
--- min year gabisa masuk ke select
--- karena kolom lain (quantity&price) gak pake agregat (perhitungan)
