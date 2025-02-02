005e7955ca1941ec26c2d7f309b9d6d46b65f75a
Cindy Meilany Fitri Andriani <86613828+cindymfa@users.noreply.github.com>
2025-02-02 22:19:36 +0700

    Time: 658 ms (87.07%), Space: 0B (100%) - LeetHub

--- Advanced String Functions-Regex-Clause/1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ Advanced String Functions-Regex-Clause/1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,4 +1,7 @@
-# Write your MySQL query statement below
-SELECT user_id, CONCAT(UPPER(LEFT(name,1)),LOWER(RIGHT(name,LENGTH(name)-1))) as name
+-- fix the names so that only the first character is uppercase and the rest are lowercase.
+SELECT 
+    user_id, 
+    CONCAT(UPPER(LEFT(name,1)),
+           LOWER(RIGHT(name,LENGTH(name)-1))) as name
 FROM Users
 ORDER BY user_id
