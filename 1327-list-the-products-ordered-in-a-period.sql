1e9d56535d70e82a96a97963f5ec89360280c471
Cindy Meilany Fitri Andriani <86613828+cindymfa@users.noreply.github.com>
2025-02-02 22:12:09 +0700

    Time: 813 ms (82.38%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period.sql
+++
@@ -1,8 +0,0 @@
--- names of products >=100 units ordered in February 2020 and their amount.
-SELECT p.product_name, sum(o.unit) unit
-FROM Products p
-JOIN Orders o
-ON p.product_id = o.product_id
-WHERE DATE_FORMAT(o.order_date,'%Y-%m') ='2020-02'
-GROUP BY o.product_id
-HAVING unit >= 100
