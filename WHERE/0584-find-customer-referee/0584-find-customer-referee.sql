545e0d1cda35d5bc8820e3631c992fef96d03c63
Cindy Meilany Fitri Andriani <86613828+cindymfa@users.noreply.github.com>
2025-02-02 22:13:35 +0700

    Time: 436 ms (98.02%), Space: 0B (100%) - LeetHub

--- WHERE/0584-find-customer-referee/0584-find-customer-referee.sql
+++ WHERE/0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,5 @@
-
-SELECT name FROM Customer
-WHERE COALESCE(referee_id,'') != 2;
+-- Write your MySQL query statement below
+SELECT name 
+FROM Customer
+WHERE COALESCE(referee_id,0) <> 2;
+-- coalesce detects nulls and replace it with 0
